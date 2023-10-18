80f2896484dc550646c8143387b9162588d28033
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-10-18 17:53:53 +0200

    Fix: "prezzo_sconto" decimals and add: output

--- js/script.js
+++ js/script.js
@@ -10,20 +10,27 @@ let prezzo_base = km_viaggio * 0.21;
 
 //Verifico e Calcolo lo Sconto
 let prezzo_sconto ;
+let sconto ;
 
 if (eta > 65) {
+    sconto = "40%"
     prezzo_sconto = prezzo_base - ( prezzo_base * 0.4);
-    prezzo_sconto = prezzo_sconto.toPrecision(2);
+    prezzo_sconto = (Math.round(prezzo_sconto * 100) / 100).toFixed(2);
     console.log(prezzo_sconto, typeof prezzo_sconto);
 } else if (eta < 18) {
+    sconto = "20%"
     prezzo_sconto = prezzo_base - ( prezzo_base * 0.2);
-    prezzo_sconto = prezzo_sconto.toPrecision(2);
+    prezzo_sconto = (Math.round(prezzo_sconto * 100) / 100).toFixed(2);
     console.log(prezzo_sconto, typeof prezzo_sconto);
 }else {
+    sconto = "Nessuno sconto per la sua fascia d'età"
     prezzo_sconto = prezzo_base;
-    prezzo_sconto = prezzo_sconto.toPrecision(2);
+    prezzo_sconto = (Math.round(prezzo_sconto * 100) / 100).toFixed(2);
     console.log(prezzo_sconto, typeof prezzo_sconto);
 }
 
 //Output
-
+document.getElementById("eta").innerHTML = eta;
+document.getElementById("km_viaggio").innerHTML = km_viaggio;
+document.getElementById("sconto").innerHTML = sconto;
+document.getElementById("prezzo_sconto").innerHTML = prezzo_sconto;
